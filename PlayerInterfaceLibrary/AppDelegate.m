d53060e8d301557395354fff65badf2df9bbd744
Giuseppe Barbalinardo <gbarbalinardo@grio.com>
2014-12-03 21:38:25 -0800

    First commit, empty app

--- PlayerInterfaceLibrary/AppDelegate.m
+++ PlayerInterfaceLibrary/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "PlayerViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,9 +17,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    self.viewController = [[PlayerViewController alloc] initWithNibName:@"PlayerViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
     return YES;
+
 }
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- PlayerInterfaceLibrary/Player/PlayerViewController.h
+++ PlayerInterfaceLibrary/Player/PlayerViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PlayerViewController.h
 //  PlayerInterfaceLibrary
 //
 //  Created by Giuseppe Barbalinardo on 12/3/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PlayerViewController : UIViewController
 
 @end
-
