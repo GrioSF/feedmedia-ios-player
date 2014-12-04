b0c0e7e829b12bb73291dec25f6520c692f8b13c
Giuseppe Barbalinardo <gbarbalinardo@grio.com>
2014-12-04 15:52:29 -0800

    Connect IBoutlet

--- PlayerInterfaceLibrary/Player Interface/BigPlayerViewController.m
+++ PlayerInterfaceLibrary/Player Interface/BigPlayerViewController.m
@@ -10,6 +10,22 @@
 #import "FMAudioPlayer.h"
 
 @interface BigPlayerViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *artistLabel;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *albumLabel;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+@property (weak, nonatomic) IBOutlet UIButton *skipButton;
+@property (weak, nonatomic) IBOutlet UIButton *thumbsUpButton;
+@property (weak, nonatomic) IBOutlet UIButton *thumbsDownButton;
+@property (weak, nonatomic) IBOutlet UIButton *volumeButton;
+@property (weak, nonatomic) IBOutlet UISlider *volumeSlider;
+- (IBAction)playButtonTouched:(id)sender;
+- (IBAction)skipButtonTouched:(id)sender;
+- (IBAction)thumbsUpButtonTouched:(id)sender;
+- (IBAction)thumbsDownButtonTouched:(id)sender;
+- (IBAction)volumeButtonTouched:(id)sender;
+- (IBAction)volumeSliderValueChanged:(id)sender;
+- (IBAction)closeButtonTouched:(id)sender;
 
 
 @end
@@ -32,4 +48,24 @@
     return YES;
 }
 
+- (IBAction)playButtonTouched:(id)sender {
+}
+
+- (IBAction)skipButtonTouched:(id)sender {
+}
+
+- (IBAction)thumbsUpButtonTouched:(id)sender {
+}
+
+- (IBAction)thumbsDownButtonTouched:(id)sender {
+}
+
+- (IBAction)volumeButtonTouched:(id)sender {
+}
+
+- (IBAction)volumeSliderValueChanged:(id)sender {
+}
+
+- (IBAction)closeButtonTouched:(id)sender {
+}
 @end
