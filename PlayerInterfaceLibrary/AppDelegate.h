4234effcb38919109a84eaab6d555a47a4132efc
Giuseppe Barbalinardo <gbarbalinardo@grio.com>
2014-12-03 21:12:14 -0800

    Initial Commit

---
+++ PlayerInterfaceLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayerInterfaceLibrary
+//
+//  Created by Giuseppe Barbalinardo on 12/3/14.
+//  Copyright (c) 2014 Grio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayerInterfaceLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayerInterfaceLibrary
+//
+//  Created by Giuseppe Barbalinardo on 12/3/14.
+//  Copyright (c) 2014 Grio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
